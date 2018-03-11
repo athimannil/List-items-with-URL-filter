369c65883f41f5e8e144aeb5c2b6036b28e91387
Muhammed Athimannil <hello@athimannil.com>
2018-03-11 23:10:02 +0100

    get news from server woth fetch()

--- client/components/newslist/Newslist.js
+++ client/components/newslist/Newslist.js
@@ -7,6 +7,24 @@ export class Newslist extends React.Component {
       news: ''
     }
   }
+
+  componentDidMount() {
+    this.requestNews();
+  }
+
+  requestNews () {
+    const urlForNews = 'http://localhost:3000/api/';
+
+    fetch(urlForNews)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return <h1>I am here for the news</h1>;
   }
