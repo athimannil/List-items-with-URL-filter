eb47e3f1c4edbf9530499aa836b5130bbec97318
Muhammed Athimannil <hello@athimannil.com>
2018-03-12 00:10:08 +0100

    tag filter on link

--- client/components/taglink/TagLink.js
+++ client/components/taglink/TagLink.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 export class TagLink extends React.Component {
+
+  filterTag = (event, link) => {
+    this.props.filterHandler(link);
+  }
+
   render () {
     return(
       <ul className="tag-links">
         {
           this.props.links.map((link, i) => {
-            return <li key={i} className="tag-links-item"><a>{link}</a></li>
+            return <li key={i} className="tag-links-item" onClick={event => this.filterTag(event, link)}><a>{link}</a></li>
           })
         }
       </ul>
